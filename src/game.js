3dbb32dbff52deb54ea4d993074b347fca4b882d
Jason <r.jason.clarke@gmail.com>
2021-10-13 13:45:09 -0500

    added score option to game

--- src/game.js
+++ src/game.js
@@ -2,11 +2,12 @@ class Game{
 
     static all = []
 
-    constructor({id, title, description, img, list_id}){
+    constructor({id, title, description, img, score, list_id}){
         this.id = id
         this.title = title
         this.description = description
         this.img = img
+        this.score = score
         this.list_id = list_id
 
         Game.all.push(this)
@@ -30,11 +31,14 @@ class Game{
         let title = document.getElementById("game-title").value
         let description = document.getElementById("game-description").value
         let img = document.getElementById("game-image").value
+        let scoreSelect = document.getElementById("game-score").options
+        let score = scoreSelect.selectedIndex + 1
         let list_id = document.getElementById("list_id").value
         let gameObject = {
             title: title,
             description: description,
             img: img,
+            score: score,
             list_id: list_id
         }
         let config = {
@@ -49,6 +53,7 @@ class Game{
         fetch(port + `/games`, config)
         .then(resp => resp.json())
         .then(gameData => {
+            debugger
             let newGame = new Game(gameData)
             newGame.addToDom()
             gameForm.style.display = "none"
@@ -69,7 +74,7 @@ class Game{
             newListBtn.style.display="none"
             gameBtns.style.display="none"
             gameForm.style.display=""
-        }) 
+        })
         gameForm.addEventListener("submit", Game.createGame)
     }
 
@@ -86,6 +91,7 @@ class Game{
         `
         <img src=${this.img}>
         <h3>${this.title}</h3>
+        <p><strong>Score: </strong>${this.score}</p>
         <p>${this.description}</p>
         <input type="hidden" value=${this.list_id} id="list_id">
         <button id=${this.id}>Delete</button>
